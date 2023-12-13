07f761d3efd0e0605928063826d0f26072246e88
kiran <kiranjojan100@gmail.com>
2023-12-13 19:02:35 +0530

    Create a New PDF files and in PDF file added invoice number

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+# this is for show all files in folders to set path
+import glob
+import openpyxl
+from fpdf import FPDF
+# this is special for file path library
+from pathlib import Path
+
+file_path = glob.glob("Invoices/*.xlsx")
+print(file_path)
+
+for second_file in file_path:
+    # pandas used read_excel to read .excel files
+    df = pd.read_excel(second_file, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P",unit="mm",format="A4")
+    pdf.add_page()
+
+    # stem give us this 10001-2023.1.18 output and cut the .extension like .xlsx
+    filename = Path(second_file).stem
+
+    # it's give the list like['10001,'2023.1.18'] and zero position is 10001
+    invoice_No = filename.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=25)
+    pdf.cell(w=0, h=8, txt=f"Invoice No.{invoice_No}")
+
+    # files name is this 10001-2023.1.18
+    pdf.output(f"PDFS/{filename}.pdf")
+
+
