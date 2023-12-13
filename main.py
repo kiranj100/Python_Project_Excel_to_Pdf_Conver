00385b71b568800dc6e67b91980b6a76ba3c620d
kiran <kiranjojan100@gmail.com>
2023-12-13 21:56:37 +0530

    add date in pdf file

--- main.py
+++ main.py
@@ -7,21 +7,34 @@ from fpdf import FPDF
 from pathlib import Path
 
 file_path = glob.glob("Invoices/*.xlsx")
-print(file_path)
+# print(file_path)
 
 for second_file in file_path:
     # pandas used read_excel to read .excel files
     df = pd.read_excel(second_file, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P",unit="mm",format="A4")
     pdf.add_page()
 
     # stem give us this 10001-2023.1.18 output and cut the .extension like .xlsx
     filename = Path(second_file).stem
 
-    # it's give the list like['10001,'2023.1.18'] and zero position is 10001
-    invoice_No = filename.split("-")[0]
-    pdf.set_font(family="Times", style="B", size=25)
-    pdf.cell(w=0, h=8, txt=f"Invoice No.{invoice_No}")
+        # it's give the list like['10001,'2023.1.18'] and zero position is 10001
+    #invoice_No = filename.split("-")[0]
+
+    # This is normal way
+    # Date = filename.split("-")[1]
+
+    # python distribute value accordingly to variables
+    invoice_no, Date = filename.split("-")
+
+    pdf.set_font(family="Times", style="B", size=18)
+    pdf.cell(w=0, h=8, txt=f"Invoice No.{invoice_no}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=18)
+    pdf.cell(w=0, h=8, txt=f"Date: {Date}")
+
+
 
     # files name is this 10001-2023.1.18
     pdf.output(f"PDFS/{filename}.pdf")
